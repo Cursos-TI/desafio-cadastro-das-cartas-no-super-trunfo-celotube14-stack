9536329c565f67af7ac146c972f10bc86eb5d755
celotube14-stack <celotube14@gmail.com>
2025-08-06 21:32:18 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,8 @@ struct CartaSuperTrunfo {
 
 int main() {
     struct CartaSuperTrunfo carta1, carta2;
+    float densidade1, densidade2;
+    float pibPerCapita1, pibPerCapita2;
 
     // Entrada de dados para a Carta 1
     printf("Cadastro da Carta 1:\n");
@@ -65,6 +67,13 @@ int main() {
     printf("Numero de Pontos Turisticos: ");
     scanf("%d", &carta2.pontosTuristicos);
 
+    // Cálculos
+    densidade1 = carta1.populacao / carta1.area;
+    densidade2 = carta2.populacao / carta2.area;
+
+    pibPerCapita1 = (carta1.pib * 1000000000) / carta1.populacao;
+    pibPerCapita2 = (carta2.pib * 1000000000) / carta2.populacao;
+
     // Impressão dos dados
     printf("\n\nCarta 1:\n");
     printf("Estado: %c\n", carta1.estado);
@@ -74,6 +83,8 @@ int main() {
     printf("Area: %.2f km²\n", carta1.area);
     printf("PIB: %.2f bilhões de reais\n", carta1.pib);
     printf("Numero de Pontos Turisticos: %d\n", carta1.pontosTuristicos);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: R$ %.2f\n", pibPerCapita1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", carta2.estado);
@@ -83,6 +94,8 @@ int main() {
     printf("Area: %.2f km²\n", carta2.area);
     printf("PIB: %.2f bilhões de reais\n", carta2.pib);
     printf("Numero de Pontos Turisticos: %d\n", carta2.pontosTuristicos);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: R$ %.2f\n", pibPerCapita2);
 
     return 0;
 }
